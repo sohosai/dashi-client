f6d94608cf57e92893d318b797854d9a5a6d8da4
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-02-28 05:42:37 +0900

    fix: remove sentry

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import * as Sentry from '@sentry/react';
 import App from './App.tsx';
 import './global.css';
 
