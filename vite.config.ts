e3a27aae03ee48f26593dc4dead7ea8b35718707
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-03-17 23:14:54 +0900

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   server: {
     host: true,
+    allowedHosts: ['localhost:5000'],
   },
   plugins: [react()],
   build: {
