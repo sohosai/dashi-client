a30a8c0a1fa36b4d697dbc16f1fffab63e843507
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-03-23 16:16:57 +0900

    feat: update report form

--- src/main.tsx
+++ src/main.tsx
@@ -13,7 +13,27 @@ Sentry.init({
     // https://docs.sentry.io/platforms/javascript/guides/react/user-feedback/configuration/#crash-report-modal
     Sentry.feedbackIntegration({
       // Additional SDK configuration goes in here, for example:
-      colorScheme: 'system',
+      colorScheme: 'light',
+      showName: true,
+      showEmail: true,
+      enableScreenshot: true,
+      isNameRequired: true,
+      isEmailRequired: true,
+      triggerLabel: '不具合を報告',
+      triggerAriaLabel: '不具合を報告',
+      formTitle: '不具合報告フォーム',
+      nameLabel: 'お名前',
+      namePlaceholder: 'お名前を入力してください',
+      emailLabel: 'メールアドレス',
+      emailPlaceholder: 'メールアドレスを入力してください',
+      messageLabel: '不具合の詳細',
+      isRequiredLabel: '（必須）',
+      messagePlaceholder: '不具合の詳細を入力してください',
+      addScreenshotButtonLabel: 'スクリーンショットを追加',
+      removeScreenshotButtonLabel: 'スクリーンショットを削除',
+      submitButtonLabel: '送信',
+      cancelButtonLabel: 'キャンセル',
+      successMessageText: 'ご協力していただき、ありがとうございました！',
     }),
   ],
 });
