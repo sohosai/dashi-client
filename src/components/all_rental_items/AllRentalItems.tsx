b6005c281819188d8d33322003b6de6b392b11a1
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-04-09 04:42:24 +0900

    feat: add style to rental list page

--- src/components/all_rental_items/AllRentalItems.tsx
+++ src/components/all_rental_items/AllRentalItems.tsx
@@ -1,12 +1,25 @@
 import { FC, useEffect, useState } from 'react';
-import { ErrorResult, ReplaceRental, UpdateRental } from '..';
+import { ErrorResult } from '..';
 import { ErrorResponse } from '../../model/errorResponse';
-import { AllRentalItemsResponse } from '../../model/allRentalItemsResponse';
+import { AllRentalItemsResponse, RentalItemResponse } from '../../model/allRentalItemsResponse';
 import { useSortRentalItem } from '../../hooks/useSortRentalItem';
+import AllRentalItemsLi from './AllRentalItemsLi';
+import styled from 'styled-components';
 
 type Props = {
   result: AllRentalItemsResponse | ErrorResponse;
 };
+
+const StyledLi = styled.li`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+`;
+
+const StyledUl = styled.ul`
+  margin: 60px 0 100px 0;
+  padding: 0;
+`;
 
 const AllRentalItemsResult: FC<Props> = (props) => {
   const [result, useResult] = useState<AllRentalItemsResponse | ErrorResponse>(props.result);
@@ -22,32 +35,13 @@ const AllRentalItemsResult: FC<Props> = (props) => {
         <ErrorResult result={result} />
       ) : (
         // fetch成功
-        result.rental_items.map((item, index) => (
-          <div key={index}>
-            <p>id: {item.id}</p>
-            <p>visible_id: {item.visible_id}</p>
-            <p>record: {item.record}</p>
-            <p>name: {item.name}</p>
-            <p>product_number: {item.product_number}</p>
-            <p>description: {item.description}</p>
-            <p>purchase_year: {item.purchase_year ?? ''}</p>
-            <p>purchase_price: {item.purchase_price ?? ''}</p>
-            <p>durability: {item.durability ?? ''}</p>
-            <p>is_depreciation: {item.is_depreciation ? '減価償却する' : '減価償却しない'}</p>
-            <p>connector: {item.connector.join(',')}</p>
-            <p>is_rent: {item.is_rent ? 'レンタル不可' : 'レンタル可'}</p>
-            <p>color: {item.color}</p>
-            <p>created_at: {item.created_at}</p>
-            <p>updated_at: {item.updated_at}</p>
-            <p>recipient: {item.recipient}</p>
-            <p>rental_description: {item.rental_description}</p>
-            <p>latest_rent_at: {item.latest_rent_at ?? ''}</p>
-            <p>scheduled_replace_at: {item.scheduled_replace_at ?? ''}</p>
-            <p>latest_replace_at: {item.latest_replace_at ?? ''}</p>
-            <UpdateRental id={item.id.toString()} isHidden={item.id === 1 || !item.is_rent ? true : false} />
-            <ReplaceRental id={item.id.toString()} isHidden={item.id === 1 || !item.is_rent ? true : false} />
-          </div>
-        ))
+        <StyledUl>
+          {result.rental_items.map((item: RentalItemResponse, index: number) => (
+            <StyledLi key={index}>
+              <AllRentalItemsLi item={item} />
+            </StyledLi>
+          ))}
+        </StyledUl>
       )}
     </>
   );
---
+++ src/components/all_rental_items/AllRentalItemsLi.tsx
@@ -0,0 +1,89 @@
+import { FC } from 'react';
+import { RentalItemResponse } from '../../model/allRentalItemsResponse';
+import { ReplaceRental, UpdateRental } from '..';
+import styled from 'styled-components';
+
+type Props = {
+  item: RentalItemResponse;
+};
+
+const StyledBox = styled.div`
+  margin: 30px auto;
+  padding: 15px;
+  width: 90%;
+  max-width: 800px;
+  background-color: #f6f6f6;
+  border: #b3b3b3 1px solid;
+`;
+
+const StyledName = styled.h2`
+  font-size: 2.8rem;
+  font-weight: 400;
+  margin: 0;
+  padding: 0;
+`;
+
+const StyledButtonWapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  gap: 15px;
+  padding: 15px 0 0 0;
+`;
+
+const StyledTable = styled.table`
+  font-size: 1.6rem;
+  margin: 0;
+  padding: 15px 15px 0 0;
+`;
+
+const StyledTdLabel = styled.td`
+  padding: 0 15px 0 0;
+`;
+
+const StyledTdBody = styled.td`
+  padding: 0 0 0 0;
+`;
+
+const AllRentalItemsLi: FC<Props> = (props) => {
+  return (
+    <StyledBox>
+      <StyledName>{props.item.name}</StyledName>
+      <StyledTable>
+        <tbody>
+          <tr>
+            <StyledTdLabel>ラベル ID</StyledTdLabel>
+            <td>{props.item.visible_id}</td>
+          </tr>
+          <tr>
+            <StyledTdLabel>借用者</StyledTdLabel>
+            <StyledTdBody>{props.item.recipient}</StyledTdBody>
+          </tr>
+          <tr>
+            <StyledTdLabel>貸し出しに関する備考</StyledTdLabel>
+            <StyledTdBody>{props.item.rental_description}</StyledTdBody>
+          </tr>
+          <tr>
+            <StyledTdLabel>最終貸し出し日時 (UTC)</StyledTdLabel>
+            <StyledTdBody>{props.item.latest_rent_at ?? ''}</StyledTdBody>
+          </tr>
+          <tr>
+            <StyledTdLabel>返却予定日 (JST)</StyledTdLabel>
+            <StyledTdBody>{props.item.scheduled_replace_at ?? ''}</StyledTdBody>
+          </tr>
+        </tbody>
+      </StyledTable>
+      <StyledButtonWapper>
+        <UpdateRental
+          id={props.item.id.toString()}
+          isHidden={props.item.id === 1 || !props.item.is_rent ? true : false}
+        />
+        <ReplaceRental
+          id={props.item.id.toString()}
+          isHidden={props.item.id === 1 || !props.item.is_rent ? true : false}
+        />
+      </StyledButtonWapper>
+    </StyledBox>
+  );
+};
+
+export default AllRentalItemsLi;
