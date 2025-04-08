84a9b78491ecf7f1b347de71657e1fa664a841ae
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-04-09 04:43:27 +0900

    feat: add css to replace item component

--- src/components/replace_rental/ReplaceRentalButton.tsx
+++ src/components/replace_rental/ReplaceRentalButton.tsx
@@ -3,6 +3,7 @@ import { OkResponse } from '../../model/okResponse';
 import { ErrorResponse } from '../../model/errorResponse';
 import { Pending } from '../../model/pending';
 import { useFetchReplaceRental } from '../../hooks/useFetchReplaceRental';
+import styled from 'styled-components';
 
 type Props = {
   id: string;
@@ -10,6 +11,14 @@ type Props = {
   setResult: Dispatch<SetStateAction<OkResponse | ErrorResponse | Pending | null>>;
   isHidden: boolean;
 };
+
+const StyledButton = styled.button`
+  padding: 5px 20px;
+  background-color: #caad63;
+  border: none;
+  font-size: 1.6rem;
+  cursor: pointer;
+`;
 
 const ReplaceRentalButton: FC<Props> = (props) => {
   const handleClick = async (): Promise<void> => {
@@ -19,11 +28,11 @@ const ReplaceRentalButton: FC<Props> = (props) => {
     props.setResult(result);
   };
   return props.isHidden ? (
-    <button onClick={handleClick} disabled>
-      Replace
-    </button>
+    <StyledButton onClick={handleClick} disabled>
+      返却
+    </StyledButton>
   ) : (
-    <button onClick={handleClick}>Replace</button>
+    <StyledButton onClick={handleClick}>返却</StyledButton>
   );
 };
 
