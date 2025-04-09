8d4702df88b950353a242f132b79e0de5f6a112f
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-04-09 19:55:55 +0900

    feat: add dashi logo

--- src/components/menu/MenuModalButton.tsx
+++ src/components/menu/MenuModalButton.tsx
@@ -1,5 +1,6 @@
 import { Dispatch, FC, SetStateAction } from 'react';
 import styled from 'styled-components';
+import DashiIcon from '/dashi.svg';
 
 type Props = {
   setIsOpen: Dispatch<SetStateAction<boolean>>;
@@ -9,8 +10,9 @@ const StyledButton = styled.button`
   height: 70px;
   width: 70px;
   border-radius: 50%;
-  background-color: #f6f6f6;
-  border: #b3b3b3 1px solid;
+  padding: 0;
+  margin: 0;
+  border: none;
   cursor: pointer;
 `;
 
@@ -18,7 +20,11 @@ const MenuModalButton: FC<Props> = (props) => {
   const handleClick = async (): Promise<void> => {
     props.setIsOpen(true);
   };
-  return <StyledButton onClick={handleClick}>Menu</StyledButton>;
+  return (
+    <StyledButton onClick={handleClick}>
+      <img src={DashiIcon} alt="menu" width={70} height={70} />
+    </StyledButton>
+  );
 };
 
 export default MenuModalButton;
