052468aecb4624829160342b7b978add092478cc
Myxogastria0808 <Myxogastria0808@users.noreply.github.com>
2025-04-10 17:55:16 +0000

    deploy: daea547e0aeb45e6864552590afaf4d84195f6b5

--- assets/Title.stories-BmbxsTTp.js
+++
@@ -1,9 +0,0 @@
-import{j as a}from"./jsx-runtime-CDGIzGgW.js";import{d}from"./styled-components.browser.esm-BTU4EXLa.js";import"./iframe-C-0cMSdF.js";import"./index-mJosnzj8.js";try{let e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{},r=new e.Error().stack;r&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[r]="8d4eb927-9e1c-4e44-8c71-144399a08bae",e._sentryDebugIdIdentifier="sentry-dbid-8d4eb927-9e1c-4e44-8c71-144399a08bae")}catch{}const l=d.h1`
-  font-size: 3.2rem;
-  font-weight: 400;
-  text-align: center;
-`,i=e=>a.jsx(l,{children:e.title});i.__docgenInfo={description:"",methods:[],displayName:"Title",props:{title:{required:!0,tsType:{name:"string"},description:""}}};const u={title:"COMPONENTS/Title",component:i,parameters:{layout:"fullscreen"}},t={args:{title:"タイトル"}};var s,n,o;t.parameters={...t.parameters,docs:{...(s=t.parameters)==null?void 0:s.docs,source:{originalSource:`{
-  args: {
-    title: 'タイトル'
-  }
-}`,...(o=(n=t.parameters)==null?void 0:n.docs)==null?void 0:o.source}}};const y=["Primary"];export{t as Primary,y as __namedExportsOrder,u as default};
