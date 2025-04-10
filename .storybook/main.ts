5aa338dcbbe25e6b06d2ce520c06ae76430182b2
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-04-11 02:35:11 +0900

    chor: update storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,18 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
+    '@chromatic-com/storybook',
+    '@storybook/experimental-addon-test',
   ],
-  "addons": [
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@chromatic-com/storybook",
-    "@storybook/experimental-addon-test"
-  ],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
-  }
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+  //画像等の読み込み
+  staticDirs: ['../public'],
 };
 export default config;
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,12 +1,24 @@
-import type { Preview } from '@storybook/react'
+import type { Preview } from '@storybook/react';
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
+    },
+    //背景色の変更
+    backgrounds: {
+      values: [
+        // 👇 Default values
+        { name: 'Dark', value: '#333' },
+        { name: 'Light', value: '#F7F9F2' },
+        // 👇 Add your own
+        { name: 'Dashi', value: '#fffdf3' },
+      ],
+      // 👇 Specify which background is shown by default
+      default: 'Dashi',
     },
   },
 };
