2004468f5998e2ad49bfdd9532b56b02ba798f9b
Myxogastria0808 <Myxogastria0808@users.noreply.github.com>
2025-04-10 19:46:11 +0000

    deploy: eb88a38e888a5828fe4171bf57a86a6775c2d6b6

---
+++ assets/ErrorResult.stories-DdXauo46.js
@@ -0,0 +1,36 @@
+import{j as r}from"./jsx-runtime-CI0FWxTJ.js";import{F as p}from"./index-3Mhajh64.js";import{d as s}from"./styled-components.browser.esm-BTZPrkTR.js";import"./iframe-CkcMkgfZ.js";import"./index-Lx1VhHd5.js";try{let e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{},t=new e.Error().stack;t&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[t]="c7a98bbd-6487-410c-9d90-0d2d6edee592",e._sentryDebugIdIdentifier="sentry-dbid-c7a98bbd-6487-410c-9d90-0d2d6edee592")}catch{}const m=s.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`,u=s.div`
+  margin: 0 auto;
+`,g=s.p`
+  margin: 0;
+  padding: 0;
+  font-size: 1.6rem;
+  text-align: center;
+`,y=s.div`
+  width: fit-content;
+  margin: 30px auto 0 auto;
+  padding: 5px 15px;
+  background-color: #f6f6f6;
+  border: 1px solid #b3b3b3;
+`,n=s.p`
+  font-size: 1.6rem;
+  margin: 5px 0 0 0;
+  padding: 0;
+`,d=s.p`
+  font-size: 1.6rem;
+  margin: 0;
+  padding: 0;
+`,c=e=>r.jsxs(m,{children:[r.jsx(u,{children:r.jsx(p,{size:90,color:"#d01c1c"})}),r.jsx(g,{children:"処理の実行に失敗しました。"}),r.jsxs(y,{children:[r.jsx(n,{children:"エラーコード"}),r.jsx(d,{children:e.result.code}),r.jsx(n,{children:"エラーメッセージ"}),r.jsx(d,{children:e.result.message})]})]});c.__docgenInfo={description:"",methods:[],displayName:"ErrorResult",props:{result:{required:!0,tsType:{name:"signature",type:"object",raw:`{
+  code: string;
+  message: string;
+}`,signature:{properties:[{key:"code",value:{name:"string",required:!0}},{key:"message",value:{name:"string",required:!0}}]}},description:""}}};const E={title:"COMPONENTS/ErrorResult",component:c,parameters:{layout:"fullscreen"}},o={args:{result:{code:"storybook/sample-error",message:"SampleError: This is sample error for storybook."}}};var i,a,l;o.parameters={...o.parameters,docs:{...(i=o.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    result: {
+      code: 'storybook/sample-error',
+      message: 'SampleError: This is sample error for storybook.'
+    }
+  }
+}`,...(l=(a=o.parameters)==null?void 0:a.docs)==null?void 0:l.source}}};const S=["Primary"];export{o as Primary,S as __namedExportsOrder,E as default};
---
+++ assets/OkResult.stories-DkCs8ZPY.js
@@ -0,0 +1,16 @@
+import{j as r}from"./jsx-runtime-CI0FWxTJ.js";import{a as l}from"./index-3Mhajh64.js";import{d as s}from"./styled-components.browser.esm-BTZPrkTR.js";import"./iframe-CkcMkgfZ.js";import"./index-Lx1VhHd5.js";try{let e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{},o=new e.Error().stack;o&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[o]="16ba2281-9f07-4be6-aabd-f59865c13e62",e._sentryDebugIdIdentifier="sentry-dbid-16ba2281-9f07-4be6-aabd-f59865c13e62")}catch{}const c=s.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`,p=s.div`
+  margin: 0 auto;
+`,u=s.p`
+  margin: 0;
+  padding: 0;
+  font-size: 1.6rem;
+  text-align: center;
+`,i=e=>r.jsxs(c,{children:[r.jsx(p,{children:r.jsx(l,{size:90,color:"#c7d01c"})}),r.jsx(u,{children:"正常に処理が実行されました。"})]});i.__docgenInfo={description:"",methods:[],displayName:"OkResult",props:{result:{required:!0,tsType:{name:"literal",value:"'ok'"},description:""}}};const x={title:"COMPONENTS/OkResult",component:i,parameters:{layout:"fullscreen"}},t={args:{result:"ok"}};var n,a,d;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    result: 'ok'
+  }
+}`,...(d=(a=t.parameters)==null?void 0:a.docs)==null?void 0:d.source}}};const _=["Primary"];export{t as Primary,_ as __namedExportsOrder,x as default};
