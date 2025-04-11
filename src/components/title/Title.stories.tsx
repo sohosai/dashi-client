8e7d2de4d596f3ceae6aabb415474f916480e901
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-04-11 15:25:04 +0900

    chor: update storybook

--- src/components/title/Title.stories.tsx
+++ src/components/title/Title.stories.tsx
@@ -17,3 +17,51 @@ export const Primary: Story = {
     title: 'タイトル',
   },
 };
+
+export const AllRentalItems: Story = {
+  args: {
+    title: '貸し出し中物品管理',
+  },
+};
+
+export const AllRentalItem: Story = {
+  args: {
+    title: '貸し出し中物品管理',
+  },
+};
+
+export const AllTrashItems: Story = {
+  args: {
+    title: '削除物品情報の履歴',
+  },
+};
+
+export const Color: Story = {
+  args: {
+    title: 'ケーブル識別色管理',
+  },
+};
+
+export const Connector: Story = {
+  args: {
+    title: '端子名管理',
+  },
+};
+
+export const Generate: Story = {
+  args: {
+    title: 'ラベル / csv の生成',
+  },
+};
+
+export const RegisterItem: Story = {
+  args: {
+    title: '物品情報の登録',
+  },
+};
+
+export const UpdateItem: Story = {
+  args: {
+    title: '物品情報の更新',
+  },
+};
