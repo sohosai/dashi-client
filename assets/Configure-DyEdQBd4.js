945610d6111b8ee139d3fe9e7c41be695ef91bbb
Myxogastria0808 <Myxogastria0808@users.noreply.github.com>
2025-04-11 07:32:18 +0000

    deploy: 33870954b8f0a3f15554ba891687d0abec00d1b1

---
+++ assets/Configure-DyEdQBd4.js
@@ -0,0 +1,31 @@
+import{j as e}from"./jsx-runtime-B_NnBnz_.js";import{useMDXComponents as s}from"./index-DjoDJzas.js";import"./iframe-IBpsA5xs.js";import"./index-BeTRQlWB.js";import"./index-Cx9kWBio.js";try{let t=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{},i=new t.Error().stack;i&&(t._sentryDebugIds=t._sentryDebugIds||{},t._sentryDebugIds[i]="20fd20f1-fec2-4516-998f-22b45b405a84",t._sentryDebugIdIdentifier="sentry-dbid-20fd20f1-fec2-4516-998f-22b45b405a84")}catch{}const o=""+new URL("dashi-Dj9XSfYL.svg",import.meta.url).href;function n(t){return e.jsxs(e.Fragment,{children:[`
+`,e.jsxs("div",{className:"container",children:[e.jsx("h1",{className:"title",children:"dashi storybook"}),e.jsx("div",{className:"logo-box",children:e.jsx("img",{src:o,width:200,height:200,alt:"dashi logo"})}),e.jsx("h2",{children:"storybookのURL"}),e.jsx("a",{href:"https://sohosai.github.io/dashi-client/",children:e.jsx("p",{className:"link",children:"https://sohosai.github.io/dashi-client/"})}),e.jsx("h2",{children:"testのcoverageのURL"}),e.jsx("a",{href:"https://sohosai.github.io/dashi-client/html/",children:e.jsx("p",{className:"link",children:"https://sohosai.github.io/dashi-client/html/"})})]}),`
+`,`
+`,e.jsx("style",{children:`
+    .sbdocs {
+      margin: 0;
+      padding: 0;
+      background-color: #fffdf3;
+    }
+    .container {
+      width: calc(100% - 40px);
+      max-width: 800px;
+      margin: 40px auto 0 auto;
+      padding: 0 20px 0 20px;
+    }
+    .title {
+      text-align: center;
+      font-size: 40px;
+      margin-bottom: 50px;
+    }
+    .logo-box {
+      display: flex;
+      justify-content: center;
+      margin: 0 0 50px 0;
+    }
+    .link {
+      font-size: 20px;
+      color:rgb(3, 174, 160);
+      text-decoration: underline;
+    }
+  `})]})}function c(t={}){const{wrapper:i}={...s(),...t.components};return i?e.jsx(i,{...t,children:e.jsx(n,{...t})}):n()}export{c as default};
---
+++ assets/SmallLoading.stories-D9ZESuwR.js
@@ -0,0 +1,43 @@
+import{j as i}from"./jsx-runtime-B_NnBnz_.js";import{d as e,m as l}from"./styled-components.browser.esm-C8Sk5b68.js";import"./iframe-IBpsA5xs.js";import"./index-BeTRQlWB.js";import"./index-Cx9kWBio.js";try{let t=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{},n=new t.Error().stack;n&&(t._sentryDebugIds=t._sentryDebugIds||{},t._sentryDebugIds[n]="96899a1f-fb7a-4e13-a0b9-b8143ce82a31",t._sentryDebugIdIdentifier="sentry-dbid-96899a1f-fb7a-4e13-a0b9-b8143ce82a31")}catch{}const m=e.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`,p=e.p`
+  margin: 0;
+  padding: 0;
+  font-size: 1.6rem;
+  text-align: center;
+`,f=l`
+    from {
+        opacity: 1;
+        scale:0;
+    }
+    to {
+        opacity: 0;
+        scale:1;
+    }
+`,g=e.div`
+  height: 90px;
+  width: 90px;
+  margin: 20px auto 20px;
+  position: relative;
+`,a=e.div`
+  height: 90px;
+  width: 90px;
+  border: 3px solid #ffffff;
+  border-radius: 50%;
+  background-color: #c7d01c;
+  animation-duration: 8s;
+  animation-iteration-count: infinite;
+  position: absolute;
+  animation-name: ${f};
+  opacity: 0;
+`,u=e(a)`
+  animation-delay: 0s;
+`,x=e(a)`
+  animation-delay: 2s;
+`,y=e(a)`
+  animation-delay: 4s;
+`,b=e(a)`
+  animation-delay: 6s;
+`,c=t=>i.jsxs(m,{children:[i.jsxs(g,{children:[i.jsx(u,{}),i.jsx(x,{}),i.jsx(y,{}),i.jsx(b,{})]}),i.jsx(p,{"data-testid":"waiting-animation",children:"処理を実行中"})]});c.__docgenInfo={description:"",methods:[],displayName:"SmallLoading"};const I={title:"COMPONENTS/loading/SmallLoading",component:c,parameters:{layout:"fullscreen"}},o={};var s,r,d;o.parameters={...o.parameters,docs:{...(s=o.parameters)==null?void 0:s.docs,source:{originalSource:"{}",...(d=(r=o.parameters)==null?void 0:r.docs)==null?void 0:d.source}}};const S=["Primary"];export{o as Primary,S as __namedExportsOrder,I as default};
