ff0ddb4c9c7096489be1ed02777baa4be563b0c5
Myxogastria0808 <Myxogastria0808@users.noreply.github.com>
2025-04-11 10:45:40 +0000

    deploy: 196cd5613cd6b43316e8b0ffdb9ec387347204e8

---
+++ assets/Barcode.stories-CDMZD6yx.js
@@ -0,0 +1,5 @@
+import{B as n}from"./Barcode-CLUfdVH9.js";import"./iframe-DJrcjO5q.js";import"./jsx-runtime-368-MtsR.js";import"./index-Ma4_De1q.js";import"./index-C4mHWc2n.js";try{let e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{},t=new e.Error().stack;t&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[t]="9b65e4f7-06ba-4f96-9288-fa67b9264246",e._sentryDebugIdIdentifier="sentry-dbid-9b65e4f7-06ba-4f96-9288-fa67b9264246")}catch{}const c={title:"COMPONENTS/generate/barcode/Barcode",component:n,parameters:{layout:"fullscreen"}},r={args:{visible_id:"0000"}};var a,o,s;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    visible_id: '0000'
+  }
+}`,...(s=(o=r.parameters)==null?void 0:o.docs)==null?void 0:s.source}}};const p=["Primary"];export{r as Primary,p as __namedExportsOrder,c as default};
