cf7dcfc739aaf4468adf83a987847ddf24171be2
Myxogastria0808 <Myxogastria0808@users.noreply.github.com>
2025-04-11 07:33:57 +0000

    deploy: 0c5c72e5a3d1aea74e83e367642ace54fa5de248

---
+++ assets/OkResult.stories-Cu2_tkm7.js
@@ -0,0 +1,16 @@
+import{j as r}from"./jsx-runtime-BL5-_QME.js";import{a as l}from"./index-YV5M1DR-.js";import{d as s}from"./styled-components.browser.esm-CKykZic0.js";import"./iframe-COM5Ta3x.js";import"./index-Cz7OxbQR.js";import"./index-BV1HRP-i.js";try{let e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{},o=new e.Error().stack;o&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[o]="b048d061-e5b7-4516-9a7a-c5a23fededc8",e._sentryDebugIdIdentifier="sentry-dbid-b048d061-e5b7-4516-9a7a-c5a23fededc8")}catch{}const c=s.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`,p=s.div`
+  margin: 0 auto;
+`,u=s.p`
+  margin: 0;
+  padding: 0;
+  font-size: 1.6rem;
+  text-align: center;
+`,i=e=>r.jsxs(c,{children:[r.jsx(p,{children:r.jsx(l,{size:90,color:"#c7d01c"})}),r.jsx(u,{children:"正常に処理が実行されました。"})]});i.__docgenInfo={description:"",methods:[],displayName:"OkResult",props:{result:{required:!0,tsType:{name:"literal",value:"'ok'"},description:""}}};const k={title:"COMPONENTS/ok/OkResult",component:i,parameters:{layout:"fullscreen"}},t={args:{result:"ok"}};var n,d,a;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    result: 'ok'
+  }
+}`,...(a=(d=t.parameters)==null?void 0:d.docs)==null?void 0:a.source}}};const _=["Primary"];export{t as Primary,_ as __namedExportsOrder,k as default};
