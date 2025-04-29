6b6481d58567649aa065eabc2f0868d1c2244538
Myxogastria0808 <Myxogastria0808@users.noreply.github.com>
2025-04-29 15:21:48 +0000

    deploy: 7ee2d27f246df7a4480e267b065e32ed54b00a8f

--- assets/Qr.stories-CH_lu0MV.js
+++
@@ -1,5 +0,0 @@
-import{Q as o}from"./Qr-CX0HfuN3.js";import"./iframe-CitulrRo.js";import"./jsx-runtime-BH-yBogc.js";import"./index-sZ8m7RlU.js";try{let e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{},t=new e.Error().stack;t&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[t]="c95961ef-b98b-425b-a5c9-12387a8950a6",e._sentryDebugIdIdentifier="sentry-dbid-c95961ef-b98b-425b-a5c9-12387a8950a6")}catch{}const b={title:"COMPONENTS/generate/qr/Qr",component:o,parameters:{layout:"fullscreen"}},r={args:{visible_id:"0000"}};var a,s,n;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
-  args: {
-    visible_id: '0000'
-  }
-}`,...(n=(s=r.parameters)==null?void 0:s.docs)==null?void 0:n.source}}};const f=["Primary"];export{r as Primary,f as __namedExportsOrder,b as default};
