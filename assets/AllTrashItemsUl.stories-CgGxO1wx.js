d5209fd2e32e2cf8d9e83c146844103b6bf91d38
Myxogastria0808 <Myxogastria0808@users.noreply.github.com>
2025-04-29 14:50:27 +0000

    deploy: 7ee2d27f246df7a4480e267b065e32ed54b00a8f

---
+++ assets/AllTrashItemsUl.stories-CgGxO1wx.js
@@ -0,0 +1,5 @@
+import{A as n}from"./AllTrashItemsUl-BFpdz3n9.js";import{a as l}from"./dummyData-B_3XOHm_.js";import"./iframe-hu0d74iQ.js";import"./jsx-runtime-CsW57hPn.js";import"./AllTrashItemsLi-Deep0qr2.js";import"./styled-components.browser.esm-BZC7IFuV.js";import"./index-CAW8tLsK.js";try{let e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{},a=new e.Error().stack;a&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[a]="4131b4b9-ba72-4515-8579-aa6e7f5921ce",e._sentryDebugIdIdentifier="sentry-dbid-4131b4b9-ba72-4515-8579-aa6e7f5921ce")}catch{}const u={title:"COMPONENTS/all_trash_items/AllTrashItemsUl",component:n,parameters:{layout:"fullscreen"}},r={args:{item:l}};var s,t,o;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    item: allTrashItemsResponseData
+  }
+}`,...(o=(t=r.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};const y=["Primary"];export{r as Primary,y as __namedExportsOrder,u as default};
--- assets/Title-DdVeIMEK.js
+++
@@ -1,5 +0,0 @@
-import{j as n}from"./jsx-runtime-BH-yBogc.js";import{d as i}from"./styled-components.browser.esm-4IHA5p08.js";import"./iframe-CitulrRo.js";try{let e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{},t=new e.Error().stack;t&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[t]="ee6cf8ae-703a-4957-9377-911042eb033a",e._sentryDebugIdIdentifier="sentry-dbid-ee6cf8ae-703a-4957-9377-911042eb033a")}catch{}const r=i.h1`
-  font-size: 3.2rem;
-  font-weight: 400;
-  text-align: center;
-`,s=e=>n.jsx(r,{children:e.title});s.__docgenInfo={description:"",methods:[],displayName:"Title",props:{title:{required:!0,tsType:{name:"string"},description:""}}};export{s as T};
