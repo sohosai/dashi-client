7b6b21d102f87461316b89a126e4fc8765795e82
Takumin-0528 <Takumin-0528@users.noreply.github.com>
2025-05-09 11:56:13 +0000

    deploy: 953c572f2ab11b71fb8ceeb28f01a210575f09cb

--- assets/Qr.stories-BLWIWwOj.js
+++
@@ -1,5 +0,0 @@
-import{Q as d}from"./Qr-DGu1jcsa.js";import"./iframe-BB6uGn-u.js";import"./jsx-runtime-Dwq2tk8t.js";import"./index-B8GNU_o8.js";import"./styled-components.browser.esm-B6tat1tx.js";try{let e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{},t=new e.Error().stack;t&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[t]="d7d1723f-7245-49f7-8be4-be86795198e5",e._sentryDebugIdIdentifier="sentry-dbid-d7d1723f-7245-49f7-8be4-be86795198e5")}catch{}const m={title:"COMPONENTS/generate/qr/Qr",component:d,parameters:{layout:"fullscreen"}},r={args:{visible_id:"0000"}};var s,o,n;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
-  args: {
-    visible_id: '0000'
-  }
-}`,...(n=(o=r.parameters)==null?void 0:o.docs)==null?void 0:n.source}}};const b=["Primary"];export{r as Primary,b as __namedExportsOrder,m as default};
