0153d944645922555d8d8f1299e9c352976248b4
Myxogastria0808 <Myxogastria0808@users.noreply.github.com>
2025-05-09 12:13:41 +0000

    deploy: 0c6b8b94f443ef0ad554a04868b8ccc01ec01c03

---
+++ assets/OkResult.stories-Mo0O4mek.js
@@ -0,0 +1,5 @@
+import{O as n}from"./OkResult-BRlb3Z7Y.js";import"./iframe-C5bPtTCz.js";import"./jsx-runtime-CzhkKXCd.js";import"./index-D6LLdZWx.js";import"./index-0_mfua7P.js";import"./styled-components.browser.esm-Bk4qQ_fP.js";try{let e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{},t=new e.Error().stack;t&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[t]="b5b665ea-ad87-4426-87d3-4d58eb0f233c",e._sentryDebugIdIdentifier="sentry-dbid-b5b665ea-ad87-4426-87d3-4d58eb0f233c")}catch{}const c={title:"COMPONENTS/ok/OkResult",component:n,parameters:{layout:"fullscreen"}},r={args:{result:"ok"}};var o,s,a;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    result: 'ok'
+  }
+}`,...(a=(s=r.parameters)==null?void 0:s.docs)==null?void 0:a.source}}};const f=["Primary"];export{r as Primary,f as __namedExportsOrder,c as default};
