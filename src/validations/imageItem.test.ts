fb715dc93b7ee92cd88aafdbefe7b208c40dcf65
Takumin-0528 <tattkktds5281sp@gmail.com>
2025-05-17 04:33:38 +0900

    test: add some test codes

--- src/validations/imageItem.test.ts
+++ src/validations/imageItem.test.ts
@@ -1,8 +1,76 @@
 import { describe, expect, test } from 'vitest';
-// import { imageItemSchema, ImageItemSchemaType } from './imageItem';
+import { imageItemSchema, ImageItemSchemaType } from './imageItem';
+import { LocalFileListBuilder } from '@mock-filelist/filelist';
 
 describe('imageItemSchemaのバリデーション', () => {
-  test.concurrent('test', () => {
-    expect(1).toBe(1);
+  test.concurrent('有効な入力: 添付画像が1枚の場合', () => {
+    let filelist: FileList = new LocalFileListBuilder()
+      .addFile({ filePath: 'test_assets/sample.png', name: 'sample.png', mimeType: 'image/png' })
+      .build();
+    const validInput: ImageItemSchemaType = {
+      image: filelist,
+    };
+    const result = imageItemSchema.safeParse(validInput);
+    expect(result.success).toBe(true);
+  });
+  test.concurrent('無効な入力: 添付画像がない場合', () => {
+    let filelist: FileList = new LocalFileListBuilder().build();
+    const validInput: ImageItemSchemaType = {
+      image: filelist,
+    };
+    const result = imageItemSchema.safeParse(validInput);
+    expect(result.success).toBe(false);
+  });
+  test.concurrent('無効な入力: 添付画像が2枚の場合', () => {
+    let filelist: FileList = new LocalFileListBuilder()
+      .addFile({ filePath: 'test_assets/sample.png', name: 'sample.png', mimeType: 'image/png' })
+      .addFile({ filePath: 'test_assets/sample.png', name: 'sample.png', mimeType: 'image/png' })
+      .build();
+    const validInput: ImageItemSchemaType = {
+      image: filelist,
+    };
+    const result = imageItemSchema.safeParse(validInput);
+    expect(result.success).toBe(false);
+  });
+  test.concurrent('無効な入力: 添付画像の容量が100MBを超える場合', () => {
+    //準備中！！
+    let filelist: FileList = new LocalFileListBuilder()
+      .addFile({ filePath: 'test_assets/heavy_sample.png', name: 'heavy_sample.png', mimeType: 'image/png' })
+      .build();
+    const validInput: ImageItemSchemaType = {
+      image: filelist,
+    };
+    const result = imageItemSchema.safeParse(validInput);
+    expect(result.success).toBe(false);
+  });
+  test.concurrent('有効な入力: 添付画像の拡張子がjpegの場合', () => {
+    let filelist: FileList = new LocalFileListBuilder()
+      .addFile({ filePath: 'test_assets/sample.jpg', name: 'sample.jpg', mimeType: 'image/jpeg' })
+      .build();
+    const validInput: ImageItemSchemaType = {
+      image: filelist,
+    };
+    const result = imageItemSchema.safeParse(validInput);
+    expect(result.success).toBe(true);
+  });
+  test.concurrent('無効な入力: 添付画像の拡張子がsvgの場合', () => {
+    let filelist: FileList = new LocalFileListBuilder()
+      .addFile({ filePath: 'test_assets/sample.svg', name: 'sample.svg', mimeType: 'image/svg+xml' })
+      .build();
+    const validInput: ImageItemSchemaType = {
+      image: filelist,
+    };
+    const result = imageItemSchema.safeParse(validInput);
+    expect(result.success).toBe(false);
+  });
+  test.concurrent('無効な入力: 添付ファイルの拡張子がtxtの場合', () => {
+    let filelist: FileList = new LocalFileListBuilder()
+      .addFile({ filePath: 'test_assets/sample.txt', name: 'sample.txt', mimeType: 'text/plain' })
+      .build();
+    const validInput: ImageItemSchemaType = {
+      image: filelist,
+    };
+    const result = imageItemSchema.safeParse(validInput);
+    expect(result.success).toBe(false);
   });
 });
--- src/validations/registerItem.test.ts
+++ src/validations/registerItem.test.ts
@@ -1,7 +1,278 @@
 import { describe, expect, test } from 'vitest';
+import { registerItemSchema, RegisterItemSchemaType } from './registerItem';
+import { LocalFileListBuilder } from '@mock-filelist/filelist';
 
 describe('registerItem.testのバリデーション', () => {
-  test.concurrent('test', () => {
-    expect(1).toBe(1);
+  test.concurrent('有効な入力: 正常に物品登録ができた場合', () => {
+    let filelist: FileList = new LocalFileListBuilder()
+      .addFile({ filePath: 'test_assets/sample.png', name: 'sample.png', mimeType: 'image/png' })
+      .build();
+    const validInput: RegisterItemSchemaType = {
+      name: 'テストアイテム',
+      visible_id: '00BB',
+      parent_visible_id: '00AA',
+      product_number: 'TEST-0000',
+      description: 'これはテストアイテムです。',
+      purchase_year: 2015,
+      purchase_price: 100,
+      durability: 5,
+      is_depreciation: true,
+      connector: [{ connector: 'HDMIオス' }, { connector: 'USB-Aオス' }],
+      color: [{ color: 'red' }, { color: 'black' }],
+      image: filelist,
+    };
+    const result = registerItemSchema.safeParse(validInput);
+    expect(result.success).toBe(true);
+  });
+  test.concurrent('無効な入力: nameが空', () => {
+    let filelist: FileList = new LocalFileListBuilder()
+      .addFile({ filePath: 'test_assets/sample.png', name: 'sample.png', mimeType: 'image/png' })
+      .build();
+    const validInput: RegisterItemSchemaType = {
+      name: '',
+      visible_id: '00BB',
+      parent_visible_id: '00AA',
+      product_number: 'TEST-0000',
+      description: 'これはテストアイテムです。',
+      purchase_year: 2015,
+      purchase_price: 100,
+      durability: 5,
+      is_depreciation: true,
+      connector: [{ connector: 'HDMIオス' }, { connector: 'USB-Aオス' }],
+      color: [{ color: 'red' }, { color: 'black' }],
+      image: filelist,
+    };
+    const result = registerItemSchema.safeParse(validInput);
+    expect(result.success).toBe(false);
+  });
+  test.concurrent('無効な入力: 物品IDが4文字以外である', () => {
+    let filelist: FileList = new LocalFileListBuilder()
+      .addFile({ filePath: 'test_assets/sample.png', name: 'sample.png', mimeType: 'image/png' })
+      .build();
+    const validInput: RegisterItemSchemaType = {
+      name: 'テストアイテム',
+      visible_id: '000BB',
+      parent_visible_id: '000AA',
+      product_number: 'TEST-0000',
+      description: 'これはテストアイテムです。',
+      purchase_year: 2015,
+      purchase_price: 100,
+      durability: 5,
+      is_depreciation: true,
+      connector: [{ connector: 'HDMIオス' }, { connector: 'USB-Aオス' }],
+      color: [{ color: 'red' }, { color: 'black' }],
+      image: filelist,
+    };
+    const result = registerItemSchema.safeParse(validInput);
+    expect(result.success).toBe(false);
+  });
+  test.concurrent('無効な入力: 物品IDに小文字の英字が含まれている', () => {
+    let filelist: FileList = new LocalFileListBuilder()
+      .addFile({ filePath: 'test_assets/sample.png', name: 'sample.png', mimeType: 'image/png' })
+      .build();
+    const validInput: RegisterItemSchemaType = {
+      name: 'テストアイテム',
+      visible_id: '00bb',
+      parent_visible_id: '00aa',
+      product_number: 'TEST-0000',
+      description: 'これはテストアイテムです。',
+      purchase_year: 2015,
+      purchase_price: 100,
+      durability: 5,
+      is_depreciation: true,
+      connector: [{ connector: 'HDMIオス' }, { connector: 'USB-Aオス' }],
+      color: [{ color: 'red' }, { color: 'black' }],
+      image: filelist,
+    };
+    const result = registerItemSchema.safeParse(validInput);
+    expect(result.success).toBe(false);
+  });
+  test.concurrent('無効な入力: 購入年度が2000年以前になっている', () => {
+    let filelist: FileList = new LocalFileListBuilder()
+      .addFile({ filePath: 'test_assets/sample.png', name: 'sample.png', mimeType: 'image/png' })
+      .build();
+    const validInput: RegisterItemSchemaType = {
+      name: 'テストアイテム',
+      visible_id: '00BB',
+      parent_visible_id: '00AA',
+      product_number: 'TEST-0000',
+      description: 'これはテストアイテムです。',
+      purchase_year: 1990,
+      purchase_price: 100,
+      durability: 5,
+      is_depreciation: true,
+      connector: [{ connector: 'HDMIオス' }, { connector: 'USB-Aオス' }],
+      color: [{ color: 'red' }, { color: 'black' }],
+      image: filelist,
+    };
+    const result = registerItemSchema.safeParse(validInput);
+    expect(result.success).toBe(false);
+  });
+  test.concurrent('無効な入力: 購入金額が0円以上でない形式になっている', () => {
+    let filelist: FileList = new LocalFileListBuilder()
+      .addFile({ filePath: 'test_assets/sample.png', name: 'sample.png', mimeType: 'image/png' })
+      .build();
+    const validInput: RegisterItemSchemaType = {
+      name: 'テストアイテム',
+      visible_id: '00BB',
+      parent_visible_id: '00AA',
+      product_number: 'TEST-0000',
+      description: 'これはテストアイテムです。',
+      purchase_year: 2015,
+      purchase_price: -100,
+      durability: 5,
+      is_depreciation: true,
+      connector: [{ connector: 'HDMIオス' }, { connector: 'USB-Aオス' }],
+      color: [{ color: 'red' }, { color: 'black' }],
+      image: filelist,
+    };
+    const result = registerItemSchema.safeParse(validInput);
+    expect(result.success).toBe(false);
+  });
+  test.concurrent('無効な入力: 耐久年数が1年以上になっていない', () => {
+    let filelist: FileList = new LocalFileListBuilder()
+      .addFile({ filePath: 'test_assets/sample.png', name: 'sample.png', mimeType: 'image/png' })
+      .build();
+    const validInput: RegisterItemSchemaType = {
+      name: 'テストアイテム',
+      visible_id: '00BB',
+      parent_visible_id: '00AA',
+      product_number: 'TEST-0000',
+      description: 'これはテストアイテムです。',
+      purchase_year: 2015,
+      purchase_price: 100,
+      durability: 0,
+      is_depreciation: true,
+      connector: [{ connector: 'HDMIオス' }, { connector: 'USB-Aオス' }],
+      color: [{ color: 'red' }, { color: 'black' }],
+      image: filelist,
+    };
+    const result = registerItemSchema.safeParse(validInput);
+    expect(result.success).toBe(false);
+  });
+  test.concurrent('無効な入力: 添付画像がない場合', () => {
+    let filelist: FileList = new LocalFileListBuilder().build();
+    const validInput: RegisterItemSchemaType = {
+      name: 'テストアイテム',
+      visible_id: '00BB',
+      parent_visible_id: '00AA',
+      product_number: 'TEST-0000',
+      description: 'これはテストアイテムです。',
+      purchase_year: 2015,
+      purchase_price: 100,
+      durability: 5,
+      is_depreciation: true,
+      connector: [{ connector: 'HDMIオス' }, { connector: 'USB-Aオス' }],
+      color: [{ color: 'red' }, { color: 'black' }],
+      image: filelist,
+    };
+    const result = registerItemSchema.safeParse(validInput);
+    expect(result.success).toBe(false);
+  });
+  test.concurrent('無効な入力: 添付画像が2枚の場合', () => {
+    let filelist: FileList = new LocalFileListBuilder()
+      .addFile({ filePath: 'test_assets/sample.png', name: 'sample.png', mimeType: 'image/png' })
+      .addFile({ filePath: 'test_assets/sample.png', name: 'sample.png', mimeType: 'image/png' })
+      .build();
+    const validInput: RegisterItemSchemaType = {
+      name: 'テストアイテム',
+      visible_id: '00BB',
+      parent_visible_id: '00AA',
+      product_number: 'TEST-0000',
+      description: 'これはテストアイテムです。',
+      purchase_year: 2015,
+      purchase_price: 100,
+      durability: 5,
+      is_depreciation: true,
+      connector: [{ connector: 'HDMIオス' }, { connector: 'USB-Aオス' }],
+      color: [{ color: 'red' }, { color: 'black' }],
+      image: filelist,
+    };
+    const result = registerItemSchema.safeParse(validInput);
+    expect(result.success).toBe(false);
+  });
+  test.concurrent('無効な入力: 添付画像の容量が100MBを超える場合', () => {
+    let filelist: FileList = new LocalFileListBuilder()
+      .addFile({ filePath: 'test_assets/heavy_sample.png', name: 'heavy_sample.png', mimeType: 'image/png' })
+      .build();
+    const validInput: RegisterItemSchemaType = {
+      name: 'テストアイテム',
+      visible_id: '00BB',
+      parent_visible_id: '00AA',
+      product_number: 'TEST-0000',
+      description: 'これはテストアイテムです。',
+      purchase_year: 2015,
+      purchase_price: 100,
+      durability: 5,
+      is_depreciation: true,
+      connector: [{ connector: 'HDMIオス' }, { connector: 'USB-Aオス' }],
+      color: [{ color: 'red' }, { color: 'black' }],
+      image: filelist,
+    };
+    const result = registerItemSchema.safeParse(validInput);
+    expect(result.success).toBe(false);
+  });
+  test.concurrent('有効な入力: 添付画像の拡張子がjpegの場合', () => {
+    let filelist: FileList = new LocalFileListBuilder()
+      .addFile({ filePath: 'test_assets/sample.jpg', name: 'sample.jpg', mimeType: 'image/jpeg' })
+      .build();
+    const validInput: RegisterItemSchemaType = {
+      name: 'テストアイテム',
+      visible_id: '00BB',
+      parent_visible_id: '00AA',
+      product_number: 'TEST-0000',
+      description: 'これはテストアイテムです。',
+      purchase_year: 2015,
+      purchase_price: 100,
+      durability: 5,
+      is_depreciation: true,
+      connector: [{ connector: 'HDMIオス' }, { connector: 'USB-Aオス' }],
+      color: [{ color: 'red' }, { color: 'black' }],
+      image: filelist,
+    };
+    const result = registerItemSchema.safeParse(validInput);
+    expect(result.success).toBe(true);
+  });
+  test.concurrent('無効な入力: 添付画像の拡張子がsvgの場合', () => {
+    let filelist: FileList = new LocalFileListBuilder()
+      .addFile({ filePath: 'test_assets/sample.svg', name: 'sample.svg', mimeType: 'image/svg+xml' })
+      .build();
+    const validInput: RegisterItemSchemaType = {
+      name: 'テストアイテム',
+      visible_id: '00BB',
+      parent_visible_id: '00AA',
+      product_number: 'TEST-0000',
+      description: 'これはテストアイテムです。',
+      purchase_year: 2015,
+      purchase_price: 100,
+      durability: 5,
+      is_depreciation: true,
+      connector: [{ connector: 'HDMIオス' }, { connector: 'USB-Aオス' }],
+      color: [{ color: 'red' }, { color: 'black' }],
+      image: filelist,
+    };
+    const result = registerItemSchema.safeParse(validInput);
+    expect(result.success).toBe(false);
+  });
+  test.concurrent('無効な入力: 添付画像の拡張子がtxtの場合', () => {
+    let filelist: FileList = new LocalFileListBuilder()
+      .addFile({ filePath: 'test_assets/sample.txt', name: 'sample.txt', mimeType: 'text/plain' })
+      .build();
+    const validInput: RegisterItemSchemaType = {
+      name: 'テストアイテム',
+      visible_id: '00BB',
+      parent_visible_id: '00AA',
+      product_number: 'TEST-0000',
+      description: 'これはテストアイテムです。',
+      purchase_year: 2015,
+      purchase_price: 100,
+      durability: 5,
+      is_depreciation: true,
+      connector: [{ connector: 'HDMIオス' }, { connector: 'USB-Aオス' }],
+      color: [{ color: 'red' }, { color: 'black' }],
+      image: filelist,
+    };
+    const result = registerItemSchema.safeParse(validInput);
+    expect(result.success).toBe(false);
   });
 });
