f05cbbf342e3626f13d2bf0ce6824ef1d12237b8
Takumin-0528 <Takumin-0528@users.noreply.github.com>
2025-05-17 05:50:19 +0000

    deploy: d4afca4873b2ad135993407f7392de42df9da795

---
+++ assets/ErrorResult.stories-Cwjxc8Zg.js
@@ -0,0 +1,8 @@
+import{E as n}from"./ErrorResult-2KATwmDW.js";import"./iframe-B54Dyf-S.js";import"./jsx-runtime-DYBgbaNi.js";import"./index-Rl4nwE6o.js";import"./index-IWdNxbET.js";import"./styled-components.browser.esm-DjErqgTs.js";try{let r=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{},o=new r.Error().stack;o&&(r._sentryDebugIds=r._sentryDebugIds||{},r._sentryDebugIds[o]="5ae60f29-ff38-48a8-a2aa-d7d5b427b124",r._sentryDebugIdIdentifier="sentry-dbid-5ae60f29-ff38-48a8-a2aa-d7d5b427b124")}catch{}const u={title:"COMPONENTS/error/ErrorResult",component:n,parameters:{layout:"fullscreen"}},e={args:{result:{code:"storybook/sample-error",message:"SampleError: This is sample error for storybook."}}};var s,a,t;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    result: {
+      code: 'storybook/sample-error',
+      message: 'SampleError: This is sample error for storybook.'
+    }
+  }
+}`,...(t=(a=e.parameters)==null?void 0:a.docs)==null?void 0:t.source}}};const b=["Primary"];export{e as Primary,b as __namedExportsOrder,u as default};
