5867b6bb524c1ab657bf8183f8e81059582b6bcc
Myxogastria0808 <Myxogastria0808@users.noreply.github.com>
2025-07-04 03:52:42 +0000

    deploy: 6cc66e39fd798bccaa8cc2e8a3573ac9614c5f68

---
+++ assets/AllTrashItemsUl.stories-D5lN33X1.js
@@ -0,0 +1,5 @@
+import{A as n}from"./AllTrashItemsUl-DmD5JVo0.js";import{a as l}from"./dummyData-Bb5n6ipd.js";import"./iframe-DuEN1TY9.js";import"./jsx-runtime-CQAveMbq.js";import"./AllTrashItemsLi-BS2lLYo_.js";import"./styled-components.browser.esm-CeEBNm1T.js";import"./index-Cygaet74.js";try{let e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{},a=new e.Error().stack;a&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[a]="4131b4b9-ba72-4515-8579-aa6e7f5921ce",e._sentryDebugIdIdentifier="sentry-dbid-4131b4b9-ba72-4515-8579-aa6e7f5921ce")}catch{}const u={title:"COMPONENTS/all_trash_items/AllTrashItemsUl",component:n,parameters:{layout:"fullscreen"}},r={args:{item:l}};var s,t,o;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    item: allTrashItemsResponseData
+  }
+}`,...(o=(t=r.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};const y=["Primary"];export{r as Primary,y as __namedExportsOrder,u as default};
