4b4424d16942f2eb33e356f9b169df653b870fa1
Myxogastria0808 <Myxogastria0808@users.noreply.github.com>
2025-07-05 11:36:38 +0000

    deploy: 949dedb51c2bb968a7219eea36708a1a5deb866e

---
+++ assets/AllTrashItemsUl.stories-DPzqvQ3F.js
@@ -0,0 +1,5 @@
+import{A as n}from"./AllTrashItemsUl-CPP4PGGH.js";import{a as l}from"./dummyData-Bdix-SKw.js";import"./iframe-CWaHC6JD.js";import"./jsx-runtime-SamgdrA_.js";import"./AllTrashItemsLi-C5NutNBb.js";import"./styled-components.browser.esm-Caf5XCC7.js";import"./index-D290XT3P.js";try{let e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{},a=new e.Error().stack;a&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[a]="4131b4b9-ba72-4515-8579-aa6e7f5921ce",e._sentryDebugIdIdentifier="sentry-dbid-4131b4b9-ba72-4515-8579-aa6e7f5921ce")}catch{}const u={title:"COMPONENTS/all_trash_items/AllTrashItemsUl",component:n,parameters:{layout:"fullscreen"}},r={args:{item:l}};var s,t,o;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    item: allTrashItemsResponseData
+  }
+}`,...(o=(t=r.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};const y=["Primary"];export{r as Primary,y as __namedExportsOrder,u as default};
---
+++ assets/ErrorResult.stories-CWWFUCp8.js
@@ -0,0 +1,8 @@
+import{E as n}from"./ErrorResult-DcQad9Sg.js";import"./iframe-CWaHC6JD.js";import"./jsx-runtime-SamgdrA_.js";import"./index-cKDNmqih.js";import"./index-D290XT3P.js";import"./styled-components.browser.esm-Caf5XCC7.js";try{let r=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{},o=new r.Error().stack;o&&(r._sentryDebugIds=r._sentryDebugIds||{},r._sentryDebugIds[o]="5ae60f29-ff38-48a8-a2aa-d7d5b427b124",r._sentryDebugIdIdentifier="sentry-dbid-5ae60f29-ff38-48a8-a2aa-d7d5b427b124")}catch{}const u={title:"COMPONENTS/error/ErrorResult",component:n,parameters:{layout:"fullscreen"}},e={args:{result:{code:"storybook/sample-error",message:"SampleError: This is sample error for storybook."}}};var s,a,t;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    result: {
+      code: 'storybook/sample-error',
+      message: 'SampleError: This is sample error for storybook.'
+    }
+  }
+}`,...(t=(a=e.parameters)==null?void 0:a.docs)==null?void 0:t.source}}};const b=["Primary"];export{e as Primary,b as __namedExportsOrder,u as default};
